8134c079e8c97789a27c4fae6e604c1376897339
Wallysson <wlc.couto@gmail.com>
2023-03-28 21:40:43 -0300

    feat(database challenge): creating database, routes and middleware'

---
+++ 01-challenge-fundamentos-nodejs/src/middlewares/routes.js
@@ -0,0 +1,46 @@
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from '../utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select(
+        'tasks',
+        search
+          ? {
+              title: search,
+              description: search
+            }
+          : null
+      )
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: new Date(),
+        updated_at: null,
+        completedAt: null
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  }
+]
