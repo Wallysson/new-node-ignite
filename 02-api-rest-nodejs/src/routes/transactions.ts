08977b99b5ca2727a656ce52db25cd83b6c8ed58
Wallysson <wlc.couto@gmail.com>
2023-05-30 19:59:55 -0300

    routes, transactions, middlewares

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,13 +1,77 @@
-import crypto from "node:crypto";
+import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import { z } from "zod";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transactions = await knex("transactions")
-      .select("*")
-      .where("id", "8b7efa2f-9282-4c41-a3f2-55cba62e26cb");
+  app.get("/", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
 
-    return transactions;
+    const transactions = await knex("transactions")
+      .where("session_id", sessionId)
+      .select();
+
+    return {
+      transactions,
+    };
+  });
+
+  app.get("/:id", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
+
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(req.params);
+
+    const transaction = await knex("transactions")
+      .select()
+      .where({ id })
+      .andWhere("session_id", sessionId)
+      .first();
+
+    return { transaction };
+  });
+
+  app.get("/summary", { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies;
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .where("session_id", sessionId)
+      .first();
+
+    return { summary };
+  });
+
+  app.post("/", { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body);
+
+    let sessionId = req.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      res.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+      });
+    }
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
+    });
+
+    return res.status(201).send();
   });
 }
